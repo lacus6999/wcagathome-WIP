ddc684e5ba28d1dc62457c2af099f055289643d1
milkyboa <61471898+milkyboa@users.noreply.github.com>
2021-06-04 23:23:49 +0200

    init

---
+++ src/test/java/com/wcagapp/wcagathome/WcagathomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcagapp.wcagathome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcagathomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
